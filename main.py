e45d50525ef21a6fcefd0ed72955a92a40c638a2
Git-Rogue <mattknight7@icloud.com>
2025-07-30 21:23:57 -0700

    Created Constants.py and Updated Main.py with basic game loop and quit event.

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import pygame
+from constants import *
+
+
 def main():
-    print("Hello from asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Game loop
+    while True:
+        # Handle events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        # Fill screen with black
+        screen.fill("black")
+        
+        # Refresh the screen
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
