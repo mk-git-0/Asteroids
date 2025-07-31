85cc298028c456baf28d14474418e2b10cada73e
Git-Rogue <mattknight7@icloud.com>
2025-07-30 21:29:20 -0700

    Added game clock and set frame rate to 60

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     
+    # Initialize delta time variable
+    dt = 0
+
     # Game loop
     while True:
         # Handle events
@@ -18,6 +22,9 @@ def main():
         
         # Refresh the screen
         pygame.display.flip()
+        
+        # Limit to 60 FPS and get delta time
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
